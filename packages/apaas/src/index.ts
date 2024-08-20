717b5db1393426fb0c9edc92fa35cb195957e526
Werheng <vhang97@163.com>
2024-08-20 17:09:59 +0800

    feat(apaas-routes): 读取路由文件

--- packages/apaas/src/index.ts
+++ packages/apaas/src/index.ts
@@ -1,6 +1,7 @@
 import process from 'node:process'
 import type { PluginOption } from 'vite'
 import { VitePluginApaasAdapter, defineConfig } from '@pubinfo/apaas-adapter'
+import { VitePluginApaasRoutes } from '@pubinfo/apaas-routes'
 
 function VitePluginApaas(): PluginOption {
   // 历史遗留问题，这里的 APAAS 环境变量是从 process.env 中获取的，不能取mode模式
@@ -8,6 +9,7 @@ function VitePluginApaas(): PluginOption {
   return isApaas
     ? [
         VitePluginApaasAdapter(),
+        VitePluginApaasRoutes(),
       ]
     : []
 }
