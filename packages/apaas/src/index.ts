b8663b412a75b747c4b7568e1a5c18d3cb4cf8c7
weiyafei <2553241022@qq.com>
2024-08-26 16:56:47 +0800

    types(apaas): vitePluginApaas 导出类型改为any

--- packages/apaas/src/index.ts
+++ packages/apaas/src/index.ts
@@ -1,9 +1,9 @@
 import process from 'node:process'
-import type { PluginOption } from 'vite'
 import { VitePluginApaasAdapter, defineConfig } from '@pubinfo/apaas-adapter'
 import { VitePluginApaasRoutes } from '@pubinfo/apaas-routes'
 
-function VitePluginApaas(): PluginOption {
+// TODO:堆栈过深跳过类型检查
+function VitePluginApaas(): any {
   // 历史遗留问题，这里的 APAAS 环境变量是从 process.env 中获取的，不能取mode模式
   const isApaas = process.env.APAAS === 'true'
   return isApaas
